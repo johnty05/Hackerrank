831edc553d20b64cf7392fb904ebc9f38a8ddfb6
Saurav Panigrahi <johntysaurav@gmail.com>
2018-05-31 08:36:33 +0530

    Add files via upload

--- DataStructures/Stacks/2.BalancedStacks.cpp
+++ DataStructures/Stacks/2.BalancedStacks.cpp
@@ -14,6 +14,8 @@ bool checkBalanced(string bracket)
 	stack<char>  S;
 	for(int i =0;i<bracket.length();i++)
 	{
+	    if(bracket[0]==')'||bracket[0]=='}'||bracket[0]==']')
+            return false;
 		if(bracket[i] == '(' || bracket[i] == '{' || bracket[i] == '[')
 			S.push(bracket[i]);
 		else if(bracket[i] == ')' || bracket[i] == '}' || bracket[i] == ']')
@@ -32,11 +34,11 @@ bool checkBalanced(string bracket)
 int main()
 {
     int n;
-	string brackString;
+	string brackStr;
     cin>>n;
     for(int i=0;i<n;i++){
-        cin>>brackString;
-        if(checkBalanced(brackString))
+        cin>>brackStr;
+        if(checkBalanced(brackStr))
 		  cout<<"YES\n";
 	    else
 		  cout<<"NO\n";
